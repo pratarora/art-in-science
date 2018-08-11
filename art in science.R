d3841b0dcd10d6308af82a82eaa36b27aed6f5f1
pratarora <pratarora@gmail.com>
2018-08-11 13:18:49 +0530

    final formatted code

--- art in science.R
+++ art in science.R
@@ -18,20 +18,17 @@ format <- ".csv"
 fulladdress <- paste(Folderadd, filename, format, sep = "")
 
 MR_data <- read.csv(fulladdress,
-         header = TRUE,
-         sep = ",") %>% na.omit() %>% arrange(desc(Freq))
+                    header = TRUE,
+                    sep = ",") %>% na.omit() %>% arrange(desc(Freq))
 
-# MR_data <- rbind(MR_data,MR_data)
-# MR_data <- rbind(MR_data,MR_data)
-# MR_data <- rbind(MR_data,MR_data)
-# MR_data <- rbind(MR_data,MR_data)
-# MR_data <- MR_data[sample(nrow(MR_data)),]
 
-# write.csv(MR_data,file= fulladdress, row.names = FALSE)
-
-figPath <- "E:/Data Science/R/art in science/inkscape png/ridge_6.png"
+figPath <-
+  "E:/Data Science/R/art in science/inkscape png/ridge_6.png"
 widget <- c(1200, 100)
-wordcloud2(MR_data, figPath = figPath, size = .1, color = "brown")
+wordcloud2(MR_data,
+           figPath = figPath,
+           size = .1,
+           color = "brown")
 
 filename <-
   "TightJunction"
@@ -39,20 +36,17 @@ format <- ".csv"
 fulladdress <- paste(Folderadd, filename, format, sep = "")
 
 TJ_data <- read.csv(fulladdress,
-                      header = TRUE,
-                      sep = ",") %>% na.omit()
-
-# TJ_data <- rbind(TJ_data,TJ_data)
-# TJ_data <- rbind(TJ_data,TJ_data)
-# TJ_data <- rbind(TJ_data,TJ_data)
-# TJ_data <- rbind(TJ_data,TJ_data)
-# TJ_data <- TJ_data[sample(nrow(TJ_data)),]
+                    header = TRUE,
+                    sep = ",") %>% na.omit()
 
 
+figPath <-
+  "E:/Data Science/R/art in science/inkscape png/tightjunction_1.png"
 
-figPath <- "E:/Data Science/R/art in science/inkscape png/tightjunction_1.png"
-
-wordcloud2(TJ_data, shape= "rectangle", size = .1, color= "red")
+wordcloud2(TJ_data,
+           shape = "rectangle",
+           size = .1,
+           color = "red")
 
 
 
@@ -65,18 +59,17 @@ fulladdress <- paste(Folderadd, filename, format, sep = "")
 AJ_data <- read.csv(fulladdress,
                     header = TRUE,
                     sep = ",") %>% na.omit()
-# 
-# AJ_data <- rbind(AJ_data,AJ_data)
-# AJ_data <- rbind(AJ_data,AJ_data)
-# AJ_data <- rbind(AJ_data,AJ_data)
-# AJ_data <- rbind(AJ_data,AJ_data)
-# AJ_data <- AJ_data[sample(nrow(AJ_data)),]
 
+figPath <-
+  "E:/Data Science/R/art in science/inkscape png/ecad_1.png"
 
-
-figPath <- "E:/Data Science/R/art in science/inkscape png/ecad_1.png"
-
-wordcloud2(AJ_data,figPath = figPath, size = .1, color= "blue", widgetsize = widget)
+wordcloud2(
+  AJ_data,
+  figPath = figPath,
+  size = .1,
+  color = "blue",
+  widgetsize = widget
+)
 
 filename <-
   "ECM"
@@ -84,16 +77,19 @@ format <- ".csv"
 fulladdress <- paste(Folderadd, filename, format, sep = "")
 
 ECM_data <- read.csv(fulladdress,
-                      header = TRUE,
-                      sep = ",") %>% na.omit()
-
-
-# ECM_data <- rbind(ECM_data,ECM_data)
+                     header = TRUE,
+                     sep = ",") %>% na.omit()
 
 
 figPath <- "E:/Data Science/R/art in science/inkscape png/ECM_2.png"
 
-wordcloud2(ECM_data, figPath= figPath, size = .1, color= "grey", widgetsize=widget)
+wordcloud2(
+  ECM_data,
+  figPath = figPath,
+  size = .1,
+  color = "grey",
+  widgetsize = widget
+)
 
 
 
@@ -103,20 +99,20 @@ format <- ".csv"
 fulladdress <- paste(Folderadd, filename, format, sep = "")
 
 mucus_data <- read.csv(fulladdress,
-                      header = TRUE,
-                      sep = ",") %>% na.omit()
-
-# MEMB_data <- rbind(MEMB_data,MEMB_data)
-# MEMB_data <- rbind(MEMB_data,MEMB_data)
-# MEMB_data <- rbind(MEMB_data,MEMB_data)
-# MEMB_data <- rbind(MEMB_data,MEMB_data)
-# MEMB_data <- MEMB_data[sample(nrow(MEMB_data)),]
+                       header = TRUE,
+                       sep = ",") %>% na.omit()
 
 
+figPath <-
+  "E:/Data Science/R/art in science/inkscape png/mucus_2.png"
 
-figPath <- "E:/Data Science/R/art in science/inkscape png/mucus_2.png"
-
-wordcloud2(mucus_data, figPath= figPath, size = .1, color= "dark green", widgetsize = widget)
+wordcloud2(
+  mucus_data,
+  figPath = figPath,
+  size = .1,
+  color = "dark green",
+  widgetsize = widget
+)
 
 filename <-
   "hemidesmosome"
@@ -124,16 +120,23 @@ format <- ".csv"
 fulladdress <- paste(Folderadd, filename, format, sep = "")
 
 hemi_data <- read.csv(fulladdress,
-                     header = TRUE,
-                     sep = ",") %>% na.omit()
+                      header = TRUE,
+                      sep = ",") %>% na.omit()
 
 
 
 
 
-figPath <- "E:/Data Science/R/art in science/inkscape png/hemidesmosome_black.png"
+figPath <-
+  "E:/Data Science/R/art in science/inkscape png/hemidesmosome_black.png"
 widget <- c(276, 68)
-wordcloud2(hemi_data, figPath= figPath, size = .1, color= "purple", widgetsize= widget)
+wordcloud2(
+  hemi_data,
+  figPath = figPath,
+  size = .1,
+  color = "purple",
+  widgetsize = widget
+)
 
 
 filename <-
@@ -149,9 +152,16 @@ desm_data <- read.csv(fulladdress,
 
 
 
-figPath <- "E:/Data Science/R/art in science/inkscape png/nucleus black_1.png"
+figPath <-
+  "E:/Data Science/R/art in science/inkscape png/nucleus black_1.png"
 widget <- c(500, 500)
-wordcloud2(desm_data, size = .1,figPath=figPath, color= "mediumspringgreen", widgetsize=widget)
+wordcloud2(
+  desm_data,
+  size = .1,
+  figPath = figPath,
+  color = "mediumspringgreen",
+  widgetsize = widget
+)
 
 
 
@@ -161,16 +171,20 @@ format <- ".csv"
 fulladdress <- paste(Folderadd, filename, format, sep = "")
 
 nuc_data <- read.csv(fulladdress,
-                      header = TRUE,
-                      sep = ",") %>% na.omit()
+                     header = TRUE,
+                     sep = ",") %>% na.omit()
 
 
 
 
 
-figPath <- "E:/Data Science/R/art in science/inkscape png/nucleus black_1.png"
+figPath <-
+  "E:/Data Science/R/art in science/inkscape png/nucleus black_1.png"
 
-wordcloud2(nuc_data, figPath= figPath , size = .1, color= "lightcoral")
+wordcloud2(nuc_data,
+           figPath = figPath ,
+           size = .1,
+           color = "lightcoral")
 
 
 filename <-
@@ -179,16 +193,23 @@ format <- ".csv"
 fulladdress <- paste(Folderadd, filename, format, sep = "")
 
 peri_data <- read.csv(fulladdress,
-                     header = TRUE,
-                     sep = ",") %>% na.omit()
+                      header = TRUE,
+                      sep = ",") %>% na.omit()
 
 
 
 
 
-figPath <- "E:/Data Science/R/art in science/inkscape png/Periderm.png"
+figPath <-
+  "E:/Data Science/R/art in science/inkscape png/Periderm.png"
 
-wordcloud2(peri_data, figPath= figPath , size = .1, color= "burlywood", widgetsize= widget)
+wordcloud2(
+  peri_data,
+  figPath = figPath ,
+  size = .1,
+  color = "burlywood",
+  widgetsize = widget
+)
 
 
 filename <-
@@ -197,13 +218,20 @@ format <- ".csv"
 fulladdress <- paste(Folderadd, filename, format, sep = "")
 
 basal_data <- read.csv(fulladdress,
-                      header = TRUE,
-                      sep = ",") %>% na.omit()
+                       header = TRUE,
+                       sep = ",") %>% na.omit()
 
 
 
 
 
-figPath <- "E:/Data Science/R/art in science/inkscape png/Basal Epidermis.png"
+figPath <-
+  "E:/Data Science/R/art in science/inkscape png/Basal Epidermis.png"
 widget <- c(1200, 330)
-wordcloud2(basal_data, figPath= figPath , size = .1, color= "chocolate", widgetsize = widget)
+wordcloud2(
+  basal_data,
+  figPath = figPath ,
+  size = .1,
+  color = "chocolate",
+  widgetsize = widget
+)
